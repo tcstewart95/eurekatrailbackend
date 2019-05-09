1602902a8342976e321e85cfd78b869e06ac7b5a
tcstewart95 <tcstewart95@gmail.com>
2019-05-09 10:54:10 -0600

    remiving extra quotes

--- theeurekatrail/database/queries.js
+++ theeurekatrail/database/queries.js
@@ -10,10 +10,10 @@ const client = mysql.createConnection({
 //creates a new player account and joins them to a company. Make sure that if they are starting a new company to create that company first. For development's sake, I am manually adding the company to the database and testing under the condition that I know the company exists.
 const createAccount = function (username, password, hp, company_id, inventory_id, role_id, image, callback) {
   console.log(hp);
-  client.query("INSERT INTO player (username, password, hp, company_id, inventory_id, role_id, image) VALUES ('"+username+"','"+password+"', '"+hp+"', '"+company_id+"', '"+inventory_id+"', '"+role_id+"', '"+image+"');"), function (err, result, fields) {
+  client.query("INSERT INTO player (username, password, hp, company_id, inventory_id, role_id, image) VALUES ('"+username+"','"+password+"', "+hp+", "+company_id+", "+inventory_id+", "+role_id+", '"+image+"');"), function (err, result, fields) {
     if (err) console.log(err);
     var today = new Date();
-    client.query("INSERT INTO plays_in(person_id, company_id, total_steps, start_date, end_date) VALUES ((SELECT id FROM player WHERE username = '"+username+"'), '"+company_id+"', '"+0+"', '"+today+"', '"+today+"');"), function (err, result, fields) {
+    client.query("INSERT INTO plays_in(person_id, company_id, total_steps, start_date, end_date) VALUES ((SELECT id FROM player WHERE username = '"+username+"'), "+company_id+", "+0+", "+today+", "+today+");"), function (err, result, fields) {
       if (err) console.log(err);
       console.log("Account Created");
       return callback(true);
