019ae2542933e5582ad9a54126143bafc777c463
tcstewart95 <tcstewart95@gmail.com>
2019-05-22 11:56:05 -0600

    Moving ) to include parameters

--- theeurekatrail/database/queries.js
+++ theeurekatrail/database/queries.js
@@ -9,18 +9,18 @@ const client = mysql.createConnection({
 
 //Checks to see if a email already exists in our system before creating a new user account
 const checkexists = function (email) {
-  client.query("SELECT * FROM player WHERE email = '"+email+"'"), function (err, result, fields) {
+  client.query("SELECT * FROM player WHERE email = '"+email+"'", function (err, result, fields) {
     if (err) console.log(err);
     return callback(true);
-  };
+  });
 }
 
 //creates a new player account and joins them to a company. Make sure that if they are starting a new company to create that company first. For development's sake, I am manually adding the company to the database and testing under the condition that I know the company exists.
 const createAccount = function (email, firstname, lastname, hp, company_id, inventory_id, role_id, image, authenticated, callback) {
-  client.query("INSERT INTO player (email, firstname, lastname, hp, company_id, inventory_id, role_id, image, authenticated) VALUES ('"+email+"', '"+firstname+"', '"+lastname+"', "+hp+", "+company_id+", "+inventory_id+", "+role_id+", '"+image+"', "+authenticated+");"), function (err, result, fields) {
+  client.query("INSERT INTO player (email, firstname, lastname, hp, company_id, inventory_id, role_id, image, authenticated) VALUES ('"+email+"', '"+firstname+"', '"+lastname+"', "+hp+", "+company_id+", "+inventory_id+", "+role_id+", '"+image+"', "+authenticated+");", function (err, result, fields) {
     if (err) console.log(err);
     return callback(true);
-  };
+  });
 }
 
 const deleteUser = function (email) {
