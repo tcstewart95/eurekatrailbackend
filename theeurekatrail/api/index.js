fdaa54068285ce4eb24b8756b843c1a0a3033609
tcstewart95 <tcstewart95@gmail.com>
2019-06-06 12:27:26 -0600

    rolling back

--- theeurekatrail/api/index.js
+++ theeurekatrail/api/index.js
@@ -185,7 +185,7 @@ router.post('/caravan/checkOwner', (req, res) => {
 
 
 router.post('/caravan/getPublic', (req, res) => {
-    db.getPublicCaravans(req.body, function(data) {
+    db.getPublicCaravans(function(data) {
         if (data) {
             res.status(201).send(data)
         } 
